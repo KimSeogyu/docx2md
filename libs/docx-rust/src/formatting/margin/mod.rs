7070f680f8dd2bec2b70661495ffa29e9155be5a
김서규 <seogyu.kim@kbfg.com>
2026-01-23 09:01:04 +0900

    chore: migrate docx-rust to external git dependency
    - Push local changes to https://github.com/KimSeogyu/docx-rs fork
    - Remove libs/docx-rust directory
    - Update Cargo.toml to use git dependency

--- libs/docx-rust/src/formatting/margin/mod.rs
+++
@@ -1,6 +0,0 @@
-mod bottom_margin;
-mod left_margin;
-mod right_margin;
-mod top_margin;
-
-pub use self::{bottom_margin::*, left_margin::*, right_margin::*, top_margin::*};
--- libs/docx-rust/src/formatting/v_merge.rs
+++
@@ -1,9 +0,0 @@
-use hard_xml::{XmlRead, XmlWrite};
-
-#[derive(Debug, Default, XmlRead, XmlWrite, Clone)]
-#[cfg_attr(test, derive(PartialEq))]
-#[xml(tag = "w:vMerge")]
-pub struct VMerge {
-    #[xml(attr = "w:val")]
-    pub val: Option<String>,
-}
